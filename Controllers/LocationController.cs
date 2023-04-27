0108e565a8e46bc05aada78e5b25ae7942aa205e
Ioana <cilteaioana@gmail.com>
2023-04-27 22:09:51 +0300

    <Random>

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -1,55 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Text.RegularExpressions;
 using TourView.Data;
 using TourView.Models;
 
 namespace TourView.Controllers
-{
-    public class LocationController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public LocationController(ApplicationDbContext context)
+        public class LocationController : Controller
         {
-            _context = context;
-        }
+            private readonly ApplicationDbContext _context;
+
+            public LocationController(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+       
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string name, string? description, string? address)
         {
-            var locations = await _context.Locations.ToListAsync();
-            return View(locations);
-        }
+            ViewData["CurrentName"] = name;
+            ViewData["CurrentDescription"] = description;
+            ViewData["CurrentAddress"] = address;
 
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            var locations = from b in _context.Locations
+                            select b;
+            if (!String.IsNullOrEmpty(name))
             {
-                return NotFound();
+                locations = locations.Where(b => b.Name.Contains(name));
+            }
+            if (!String.IsNullOrEmpty(description))
+            {
+                locations = locations.Where(b => b.Description.Contains(description));
+            }
+            if (!String.IsNullOrEmpty(address))
+            {
+                locations = locations.Where(b => b.Address.Contains(address));
             }
 
-            var location = await _context.Locations.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (location == null)
+            int _perPage = 3;
+            if (TempData.ContainsKey("message"))
             {
-                return NotFound();
-            }
-
-            return View(location);
-        }
-
-        public async Task<IActionResult> Index(string searchString)
-        {
-            IQueryable<Location> locations = _context.Locations;
-
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                locations = locations.Where(l => l.Name.Contains(searchString));
+                ViewBag.message = TempData["message"].ToString();
             }
 
             return View(await locations.ToListAsync());
         }
 
+
+
+        public async Task<IActionResult> Details(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var location = await _context.Locations.FirstOrDefaultAsync(m => m.Id == id);
+
+                if (location == null)
+                {
+                    return NotFound();
+                }
+
+                return View(location);
+            }
+
+        }
     }
-}
