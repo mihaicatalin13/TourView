ff87db2e3e163aa7f2130f3d445345a94916c0ee
Adrian-Ionut Sefcic <adriansefcic@yahoo.ro>
2023-06-15 01:59:08 +0300

    before starting whatever manager means, fixed location edit/delete, upload location image and some more bugs, might need to be thoroughly checked for bugs again

---
+++ Models/LocationPhoto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TourView.Models
+{
+    public class LocationPhoto
+    {
+        [Key]
+        public int PhotoId { get; set; }
+        public string LocationId { get; set; } = string.Empty;
+        public string ImageName { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+    }
+}
