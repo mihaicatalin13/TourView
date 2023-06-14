df763b9f718c15c66b834990be3cf845ca3f5c05
Adrian-Ionut Sefcic <adriansefcic@yahoo.ro>
2023-06-14 18:30:55 +0300

    added ioana's branch for merge

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -10,10 +11,15 @@ namespace TourView.Controllers
     public class LocationController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public LocationController(ApplicationDbContext context)
+        public LocationController(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
         {
             _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         [HttpGet]
@@ -64,22 +70,28 @@ namespace TourView.Controllers
         [Authorize(Roles = "Editor")]
         public IActionResult Create()
         {
-            return View();
+            Location location = new Location();
+            return View(location);
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         [Authorize(Roles = "Editor")]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Address,PhoneNumber,Schedule,Menu,PhotoUrl,Rating")] Location location)
+        public async Task<IActionResult> Create(Location location)
         {
+            location.ManagerId = _userManager.GetUserId(User);
             if (ModelState.IsValid)
             {
-                _context.Add(location);
+                _context.Locations.Add(location);
                 await _context.SaveChangesAsync();
+                TempData["message"] = "Location loaded";
                 return RedirectToAction(nameof(Index));
             }
             return View(location);
+
+
         }
+
 
         [Authorize(Roles = "Editor")]
         public async Task<IActionResult> Edit(int? id)
