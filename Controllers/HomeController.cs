954037acf77c43b044d0ccc42b4b0201fc26927e
Adrian-Ionut Sefcic <adriansefcic@yahoo.ro>
2023-06-15 15:19:08 +0300

    added HomePage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TourView.Data;
 using TourView.Models;
 
 namespace TourView.Controllers
@@ -7,15 +9,29 @@ namespace TourView.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            Review fiveStarRev;
+            int fiveStarLocId;
+            Location location;
+            if (_context.Reviews.FirstOrDefault(r => r.Rating == 5) != null)
+            {
+                fiveStarRev = _context.Reviews.OrderBy(rev => rev.Id).Last(rev => rev.Rating == 5);
+                fiveStarLocId = _context.Reviews.OrderBy(rev => rev.Id).Last(rev => rev.Rating == 5).LocationId;
+                location = _context.Locations.Find(fiveStarLocId);
+                ViewBag.review = fiveStarRev;
+                ViewBag.userName = _context.Users.First(u => u.Id == fiveStarRev.UserId).UserName;
+                return View(location);
+            }
+            return View(null);
         }
 
         public IActionResult Privacy()
